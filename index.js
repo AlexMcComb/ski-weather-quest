4adb7b4201ee15b3e0e272ad5e617286485a1fd8
AlexMcComb <alexandermccomb@gmail.com>
2018-11-20 13:31:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const contentWeather = document.querySelector('.results-output');
 
 // import key
 const searchURL = 'https://api.openweathermap.org/data/2.5/weather';
-const myKey = '';
+const myKey = 'b665746e57c457076f042fba17e9bb15';
 const imp = 'imperial';
 
 
